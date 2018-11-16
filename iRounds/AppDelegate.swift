6c6454f80a8b546240e5bdeaa0136f0be133d0b5
sd.trent@yahoo.com <sd.trent@yahoo.com>
2018-11-15 22:34:46 -0700

    first commit

--- iRounds/AppDelegate.swift
+++ iRounds/AppDelegate.swift
@@ -7,6 +7,13 @@
 //
 
 import UIKit
+import Firebase
+
+
+let db = Firestore.firestore()
+
+let settings = db.settings
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +23,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        settings.areTimestampsInSnapshotsEnabled = true
+        db.settings = settings
         return true
     }
 
--- iRounds/ViewController.swift
+++ iRounds/ViewController.swift
@@ -7,12 +7,47 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    var ref: DocumentReference? = nil
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //signing in
+        Auth.auth().signIn(withEmail: "sd.trent@yahoo.com", password: "thatoneguY1") { (result, error) in
+            if let error = error
+            {
+                print("there was a problem logging the user in...\(error.localizedDescription)")
+            }
+            else
+            {
+                print("Logged in user \(String(describing: result?.user.email!))")
+            }
+            
+            
+        }
+        
+        //getting employee data
+        db.collection("employees").getDocuments() { (querySnapshot, error) in
+            if let error = error
+            {
+                print("there was an error getting the docs...\(error.localizedDescription)")
+            }
+            else
+            {
+                for document in querySnapshot!.documents
+                {
+                    print("\(document.documentID) => \(document.data())")
+                }
+            }
+        }
+        
+        
     }
 
 
